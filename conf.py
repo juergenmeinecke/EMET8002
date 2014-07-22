f1a1e9c48d7c3aa19bae82eeead97086ba20ceeb
Juergen Meinecke <juergen.meinecke@gmail.com>
2014-07-22 14:19:42 +1000

    Updated, fixed conf.py file

--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.mathjax']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -193,7 +193,8 @@ latex_elements = {
 #'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-#'preamble': '',
+'preamble': '\usepackage{amsmath, amsthm, amscd, amsfonts, amssymb, amsbsy, mathpazo}',
+
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
